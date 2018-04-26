9e582b18eb3c6c56e2a4ee177b1392296fa73507
Saul <rscr1@gcloud.ua.es>
2018-04-26 10:21:06 +0200

    Add function service

--- src/controller.cpp
+++ src/controller.cpp
@@ -4,13 +4,29 @@
 #include "std_msgs/String.h"
 #include "std_msgs/Bool.h"
 #include "lights_control/LightingControl.h"
+#include "lights_control/ChangeLightStatus.h"
+
+
+bool toogleLight(lights_control::ChangeLightStatus::Request &req,
+								 lights_control::ChangeLightStatus::Response &res)
+{
+	return true;
+}
+
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "controller");
-	ros::NodeHandle n;
-	ros::Publisher lights_status_pub = n.advertise<lights_control::LightingControl>("lights_status", 1000);
 	ros::Rate loop_rate(0.5);
+
+	ros::NodeHandle n;
+
+	ros::Publisher lights_status_pub = n.advertise<lights_control::LightingControl>("lights_status", 1000);
+	ROS_INFO("Ready /lights_status Topic");
+
+	ros::ServiceServer service = n.advertiseService("change_light_status", toogleLight);
+  ROS_INFO("Ready /change_light_status Service");
+
 
 	lights_control::LightingControl msg;
 
