0afa1fc45a1e419cc9f90581be98d29e61a32d43
Saul <rscr1@gcloud.ua.es>
2018-04-26 10:50:34 +0200

    Firts fine implementation of service

--- src/controller.cpp
+++ src/controller.cpp
@@ -7,9 +7,22 @@
 #include "lights_control/ChangeLightStatus.h"
 
 
+lights_control::LightingControl msg;
+
 bool toogleLight(lights_control::ChangeLightStatus::Request &req,
-								 lights_control::ChangeLightStatus::Response &res)
+								 lights_control::ChangeLightStatus::Response &res
+							   )
 {
+	if(msg.id == req.id)
+	{
+		msg.state = req.state;
+		if(msg.state == req.state)
+			res.result = res.OK;
+	}
+
+	else
+		res.result = res.ERROR;
+
 	return true;
 }
 
@@ -17,21 +30,18 @@ bool toogleLight(lights_control::ChangeLightStatus::Request &req,
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "controller");
-	ros::Rate loop_rate(0.5);
-
 	ros::NodeHandle n;
 
 	ros::Publisher lights_status_pub = n.advertise<lights_control::LightingControl>("lights_status", 1000);
 	ROS_INFO("Ready /lights_status Topic");
 
-	ros::ServiceServer service = n.advertiseService("change_light_status", toogleLight);
+	ros::ServiceServer service = n.advertiseService("change_light_status",toogleLight);
   ROS_INFO("Ready /change_light_status Service");
 
+	ros::Rate loop_rate(0.5);
 
-	lights_control::LightingControl msg;
-
-	msg.id_number = 10;
-	msg.id_name = "luces_habitacion";
+	msg.id = 10;
+	msg.name = "luces_habitacion";
 	msg.state = msg.ON;
 
 	while (ros::ok())
