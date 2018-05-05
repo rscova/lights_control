512b74310e4a4a3cd513af78307c5111c89f2e8b
Saul <rscr1@gcloud.ua.es>
2018-05-06 01:09:07 +0200

    Filtered mqtt msgs using json

--- scripts/mqtt_subscriber.py
+++ scripts/mqtt_subscriber.py
@@ -11,11 +11,12 @@ data_string =  str(data.id) + " " + data.name + " "+str(data.state)
 
 
 def on_message(client, userdata, message):
-    rospy.loginfo("message received: %s" ,str(message.payload.decode("utf-8")))
-    rospy.loginfo("message topic: %s",message.topic)
-    rospy.loginfo("message qos: %s",message.qos)
-    rospy.loginfo("message retain flag: %s",message.retain)
-
+    m_decode = str(message.payload.decode("utf-8"))
+    m_in = json.loads(m_decode)
+    #rospy.loginfo("message received: %s" ,m_in)
+    #rospy.loginfo("message received: %s" ,str(message.payload.decode("utf-8")))
+    rospy.loginfo("message received: %s %s %s", m_in["id"], m_in["name"], m_in["state"])
+    rospy.loginfo("message topic: %s \n",message.topic)
 
 broker_address="192.168.1.4"
 port = 1883
@@ -29,7 +30,6 @@ def mqtt_subscriber():
 
     rospy.init_node('mqtt_subscriber', anonymous=True)
     client.subscribe("json")
-    rospy.loginfo("Subscribed")
     rospy.spin()
 
 if __name__ == '__main__':
